0a19bebec329ffd75483deb4e7bbbd98c270216a
Alexandre Devilliers <elbereth@dashninja.pl>
2004-07-15 16:38:42 +0000

    Stripped down version. No more binary read/write functions (now in lib_Binutils). NOTE: This might have broken compilation for some parts of Dragon UnPACKer and will be fixed time permiting.

--- DragonUnPACKer/common/lib_Utils.pas
+++ DragonUnPACKer/common/lib_Utils.pas
@@ -1,6 +1,6 @@
 unit lib_Utils;
 
-// $Id: lib_Utils.pas,v 1.2 2004-07-14 19:18:02 elbereth Exp $
+// $Id: lib_Utils.pas,v 1.3 2004-07-15 16:38:42 elbereth Exp $
 // $Source: /home/elbzone/backup/cvs/DragonUnPACKer/common/lib_Utils.pas,v $
 //
 // The contents of this file are subject to the Mozilla Public License
@@ -63,19 +63,9 @@ function BoolToStr(B: Boolean) : string;
 function splitStr(Chaine: string; Casseur: string): TStringList;
 function DescFromExt(ext: string): string;
 function IconFromExt(ext: string): integer;
-function posrev(substr: string; str: string): integer;
-function strip0(str : string): string;
 function CheckOS(): boolean;
 function getOS(): integer;
-function Get0(src: integer): string;
-function Get8(src: integer): string;
-function Get8u(src: integer): string;
-function Get8v(src: integer; size: byte): string;
-function Get16v(src: integer; size: word): string;
-function Get32(src: integer): string;
 function PadRight(Data: string; PadWidth: integer) : string;
-procedure put8(src: integer; val: string);
-function revstr(str: string): string;
 procedure SetRegistryDUP5();
 procedure SetRegistryDUPPI();
 procedure SetRegistryType(ext: string);
@@ -87,7 +77,7 @@ function GetAllSystemInfo(): TOSInfo;
 
 implementation
 
-uses SysUtils, lib_language, Windows, registry, forms;
+uses SysUtils, lib_language, Windows, registry, forms, lib_BinUtils;
 
 function PadRight(Data: string; PadWidth: integer) : string;
 begin
@@ -301,158 +291,6 @@ begin
     IconFromExt := 14
   else
     IconFromExt := 0;
-
-end;
-
-function strip0(str : string): string;
-var pos0: integer;
-begin
-
-  pos0 := pos(chr(0),str);
-
-  if pos0 > 0 then
-    strip0 := copy(str, 1, pos0 - 1)
-  else
-    strip0 := str;
-
-end;
-
-function revstr(str: string): string;
-var res: string;
-    x: integer;
-begin
-
-  for x := 0 to length(str)-1 do
-    res := str[x]+res;
-
-  revstr := res;
-
-end;
-
-function posrev(substr: string; str: string): integer;
-var res,x : integer;
-begin
-
-  res := 0;
-  x := (length(str) - length(substr) + 1);
-
-  while (x >= 1) and (res = 0) do
-  begin
-
-    if copy(str,x, length(substr)) = substr then
-      res := x;
-
-    x := x - 1;
-
-  end;
-
-  posrev := res;
-
-end;
-
-procedure put8(src: integer; val: string);
-var tchar: array[0..255] of Char;
-    tbyt: byte;
-    x: integer;
-begin
-
-  if (length(val) > 256) then
-    val := LeftStr(val,256);
-
-  FillChar(tchar,256,0);
-
-  for x := 0 to length(val)-1 do
-    tchar[x] := val[x+1];
-
-  tbyt := length(val);
-
-  FileWrite(src,tbyt,1);
-  if (tbyt > 0) then
-        FileWrite(src,tchar,length(val));
-
-end;
-
-function Get8(src: integer): string;
-var tchar: array[0..255] of Char;
-    tbyt: byte;
-begin
-
-  FileRead(src,tbyt,1);
-  FillChar(tchar,256,0);
-  // Fixed bug #958622 : this may have fixed other functions relying on get8
-  FileRead(src,tchar[0],tbyt);
-
-  Get8 := tchar;
-
-end;
-
-function Get8u(src: integer): string;
-var tchar: WideString;
-    tbyt: byte;
-begin
-
-  FileRead(src,tbyt,1);
-  setlength(tchar,tbyt);
-//  FillChar(tchar,256,0);
-  FileRead(src,tchar[1],tbyt*2);
-
-  result := tchar;
-
-end;
-
-function Get32(src: integer): string;
-var tchar: Pchar;
-    tint: Integer;
-begin
-
-  FileRead(src,tint,4);
-  GetMem(tchar,tint);
-  FillChar(tchar^,tint,0);
-  FileRead(src,tchar^,tint);
-
-  Get32 := tchar;
-
-  FreeMem(tchar);
-
-end;
-
-function Get0(src: integer): string;
-var tchar: Char;
-    res: string;
-begin
-
-  repeat
-    FileRead(src,tchar,1);
-    res := res + tchar;
-  until tchar = chr(0);
-
-  Get0 := res;
-
-end;
-
-function Get8v(src: integer; size: byte): string;
-var tchar: array[0..255] of Char;
-    res: string;
-begin
-
-  FillChar(tchar,256,0);
-  FileRead(src,tchar,size);
-
-  res := tchar;
-  Get8v := Copy(res,1,size);
-
-end;
-
-function Get16v(src: integer; size: word): string;
-var tchar: array[1..1024] of Char;
-    res: string;
-begin
-
-  FillChar(tchar,1023,0);
-  FileRead(src,tchar,size);
-
-  res := tchar;
-  Get16v := Copy(res,1,size);
 
 end;
 

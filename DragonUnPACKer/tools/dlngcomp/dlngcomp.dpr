17f95f6f747f9fce083ed5306972c00099aee5ea
Alexandre Devilliers <elbereth@dashninja.pl>
2004-12-22 19:43:27 +0000

    License

--- DragonUnPACKer/tools/dlngcomp/dlngcomp.dpr
+++ DragonUnPACKer/tools/dlngcomp/dlngcomp.dpr
@@ -2,6 +2,23 @@ program dlngcomp;
 
 {$APPTYPE CONSOLE}
 
+// $Id: dlngcomp.dpr,v 1.1.2.3 2004-12-22 19:43:27 elbereth Exp $
+// $Source: /home/elbzone/backup/cvs/DragonUnPACKer/tools/dlngcomp/dlngcomp.dpr,v $
+//
+// The contents of this file are subject to the Mozilla Public License
+// Version 1.1 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+// specific language governing rights and limitations under the License.
+//
+// The Original Code is dlngcomp.dpr, released December 22, 2004.
+//
+// The Initial Developer of the Original Code is Alexandre Devilliers
+// (elbereth@users.sourceforge.net, http://www.elberethzone.net).
+//
+
 uses
   SysUtils,
   Classes,

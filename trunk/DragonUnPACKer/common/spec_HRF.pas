be174e00c60ace4657e4a631e2bfb11f0a796765
elbereth <elbereth@d7b88232-77aa-4384-a92c-d688c1a3d038>
2008-03-06 19:38:58 +0000

    HyperRipper 5.5a: Fixed the way threading was used! This fixes some Richtext error I got while using old version.  This should also have definitely fixed 1686603 (Problem with AMD Dual Core CPU's)  and now Dragon UnPACKer just works flawlessly with multi-core/cpu affinity set.

--- trunk/DragonUnPACKer/common/spec_HRF.pas
+++ trunk/DragonUnPACKer/common/spec_HRF.pas
@@ -1,6 +1,6 @@
 unit spec_HRF;
 
-// $Id: spec_HRF.pas,v 1.5 2008-03-04 06:18:33 elbereth Exp $
+// $Id: spec_HRF.pas,v 1.6 2008-03-06 19:38:58 elbereth Exp $
 // $Source: /home/elbzone/backup/cvs/DragonUnPACKer/common/spec_HRF.pas,v $
 //
 // The contents of this file are subject to the Mozilla Public License
@@ -41,7 +41,7 @@ unit spec_HRF;
 
 interface
 
-const HR_VERSION = 55040;	// HyperRipper version
+const HR_VERSION = 55041;	// HyperRipper version
       HR_ID = 1;
 
       HR_TYPE_ERROR = -1;

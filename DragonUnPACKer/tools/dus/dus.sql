9ca440248ce3863f30e689f8ca7794b5db693e60
Alexandre Devilliers <elbereth@dashninja.pl>
2008-10-04 08:56:33 +0000

    Updated database structure for DUS v3.1.6 (multi-servers & unstable support)

--- DragonUnPACKer/tools/dus/dus.sql
+++ DragonUnPACKer/tools/dus/dus.sql
@@ -1,27 +1,54 @@
+-- $Id: dus.sql,v 1.2 2008-10-04 08:56:33 elbereth Exp $
+-- $Source: /home/elbzone/backup/cvs/DragonUnPACKer/tools/dus/dus.sql,v $
+--
+-- The contents of this file are subject to the Mozilla Public License
+-- Version 1.1 (the "License"); you may not use this file except in compliance
+-- with the License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+--
+-- Software distributed under the License is distributed on an "AS IS" basis,
+-- WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+-- specific language governing rights and limitations under the License.
+--
+-- The Original Code is dus.sql, released December 17, 2005.
+--
+-- The Initial Developer of the Original Code is Alexandre Devilliers
+-- (elbereth@users.sourceforge.net, http://www.elberethzone.net).
+--
+
 -- phpMyAdmin SQL Dump
--- version 2.7.0-pl1
+-- version 2.11.7
 -- http://www.phpmyadmin.net
--- 
--- Host: pr-db2
--- Generation Time: Dec 16, 2005 at 03:58 PM
--- Server version: 4.1.12
--- PHP Version: 4.3.9
--- 
+--
+-- Host: localhost
+-- Generation Time: Oct 04, 2008 at 08:51 AM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
 -- Database: `d108923_DragonUnPACKer`
--- 
+--
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `dus_convert`
--- 
+--
 
 CREATE TABLE `dus_convert` (
-  `name` varchar(12) NOT NULL default '',
+  `name` varchar(12) NOT NULL,
   `version` int(11) NOT NULL default '0',
+  `versiontype` enum('stable','unstable') NOT NULL default 'stable',
   `description` text NOT NULL,
   `duci` tinyint(4) NOT NULL default '0',
-  `versiondisp` varchar(32) NOT NULL default '',
+  `versiondisp` varchar(32) NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
@@ -30,37 +57,40 @@ CREATE TABLE `dus_convert` (
   `commentFR` text NOT NULL,
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`name`,`version`),
-  KEY `duci` (`duci`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `duci` (`duci`),
+  KEY `versiontype` (`versiontype`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `dus_core`
--- 
+--
 
 CREATE TABLE `dus_core` (
   `build` int(11) NOT NULL default '0',
   `date` date NOT NULL default '0000-00-00',
-  `type` enum('wip','stable') NOT NULL default 'wip',
-  `versiondisp` varchar(64) NOT NULL default '',
-  `URL` text NOT NULL,
-  `available` enum('no','yes') NOT NULL default 'no',
+  `type` enum('wip','stable') character set utf8 NOT NULL default 'wip',
+  `versiondisp` varchar(64) character set utf8 NOT NULL,
+  `URL` text character set utf8 NOT NULL,
+  `UpdateURL` text character set utf8,
+  `available` enum('no','yes') character set utf8 NOT NULL default 'no',
   PRIMARY KEY  (`build`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `dus_driver`
--- 
+--
 
 CREATE TABLE `dus_driver` (
-  `name` varchar(12) NOT NULL default '',
+  `name` varchar(12) NOT NULL,
   `version` int(11) NOT NULL default '0',
+  `versiontype` enum('stable','unstable') NOT NULL default 'stable',
   `description` text NOT NULL,
   `dudi` tinyint(4) NOT NULL default '0',
-  `versiondisp` varchar(32) NOT NULL default '',
+  `versiondisp` varchar(32) NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
@@ -70,20 +100,21 @@ CREATE TABLE `dus_driver` (
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`name`,`version`),
   KEY `dudi` (`dudi`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `dus_hyperripper`
--- 
+--
 
 CREATE TABLE `dus_hyperripper` (
-  `name` varchar(12) NOT NULL default '',
+  `name` varchar(12) NOT NULL,
   `version` int(11) NOT NULL default '0',
+  `versiontype` enum('stable','unstable') NOT NULL default 'stable',
   `description` text NOT NULL,
   `duhi` tinyint(4) NOT NULL default '0',
-  `versiondisp` varchar(32) NOT NULL default '',
+  `versiondisp` varchar(32) NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
@@ -92,41 +123,62 @@ CREATE TABLE `dus_hyperripper` (
   `commentFR` text NOT NULL,
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`name`,`version`),
-  KEY `duhi` (`duhi`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `duhi` (`duhi`),
+  KEY `versiontype` (`versiontype`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
--- 
+--
+-- Table structure for table `dus_servers`
+--
+
+CREATE TABLE `dus_servers` (
+  `serverID` tinyint(4) NOT NULL,
+  `serverPriority` int(11) NOT NULL default '0',
+  `serverCountry` varchar(2) character set ascii NOT NULL default 'fr',
+  `serverURL` text NOT NULL,
+  `serverName` varchar(128) NOT NULL,
+  `serverEnabled` enum('true','false') character set ascii NOT NULL default 'true',
+  `serverUsePaths` enum('true','false') NOT NULL default 'true',
+  PRIMARY KEY  (`serverID`),
+  KEY `serverPriority` (`serverPriority`,`serverEnabled`),
+  KEY `serverUsePaths` (`serverUsePaths`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `dus_translation`
--- 
+--
 
 CREATE TABLE `dus_translation` (
-  `name` varchar(32) NOT NULL default '',
+  `name` varchar(32) NOT NULL,
   `version` tinyint(4) NOT NULL default '0',
   `release` tinyint(4) NOT NULL default '0',
-  `description` varchar(128) NOT NULL default '',
+  `description` varchar(128) NOT NULL,
   `author` text NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
   `size` int(11) NOT NULL default '0',
-  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`name`,`version`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `date` date NOT NULL default '0000-00-00',
+  PRIMARY KEY  (`name`,`version`,`release`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `dus_versions`
--- 
+--
 
 CREATE TABLE `dus_versions` (
-  `dup` int(11) NOT NULL default '0',
+  `dupfrom` int(11) NOT NULL,
+  `dupto` int(11) NOT NULL,
   `duci` int(11) NOT NULL default '0',
   `dudi` int(11) NOT NULL default '0',
   `duhi` int(11) NOT NULL default '0',
   `translation` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`dup`),
+  PRIMARY KEY  (`dupfrom`,`dupto`),
   KEY `convert` (`duci`,`dudi`,`duhi`,`translation`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;

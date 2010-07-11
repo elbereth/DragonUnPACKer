cb05c83cf1cf73d777c11555ee25a9d8883b97cb
Alexandre Devilliers <elbereth@dashninja.pl>
2010-07-11 15:20:41 +0000

    Updated to include hash (SHA-1) & real file size for all D5P files

--- DragonUnPACKer/tools/dus/dus.sql
+++ DragonUnPACKer/tools/dus/dus.sql
@@ -1,5 +1,3 @@
--- $Id: dus.sql,v 1.4 2009-07-11 14:06:36 elbereth Exp $
--- $Source: /home/elbzone/backup/cvs/DragonUnPACKer/tools/dus/dus.sql,v $
 --
 -- The contents of this file are subject to the Mozilla Public License
 -- Version 1.1 (the "License"); you may not use this file except in compliance
@@ -16,21 +14,15 @@
 --
 
 -- phpMyAdmin SQL Dump
--- version 2.11.7
+-- version 3.3.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 15, 2008 at 06:39 PM
--- Server version: 5.0.51
--- PHP Version: 5.2.6
+-- Generation Time: Jul 11, 2010 at 03:18 PM
+-- Server version: 5.1.47
+-- PHP Version: 5.3.2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `d108923_DragonUnPACKer`
@@ -44,19 +36,19 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE `dus_convert` (
   `name` varchar(12) NOT NULL,
-  `version` int(11) NOT NULL default '0',
-  `versiontype` enum('stable','unstable') NOT NULL default 'stable',
+  `version` int(11) NOT NULL DEFAULT '0',
+  `versiontype` enum('stable','unstable') NOT NULL DEFAULT 'stable',
   `description` text NOT NULL,
-  `duci` tinyint(4) NOT NULL default '0',
+  `duci` tinyint(4) NOT NULL DEFAULT '0',
   `versiondisp` varchar(32) NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
-  `size` int(11) NOT NULL default '0',
+  `size` int(11) NOT NULL DEFAULT '0',
   `comment` text NOT NULL,
   `commentFR` text NOT NULL,
-  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`name`,`version`),
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`name`,`version`),
   KEY `duci` (`duci`),
   KEY `versiontype` (`versiontype`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -68,14 +60,14 @@ CREATE TABLE `dus_convert` (
 --
 
 CREATE TABLE `dus_core` (
-  `build` int(11) NOT NULL default '0',
-  `date` date NOT NULL default '0000-00-00',
-  `type` enum('wip','stable') character set utf8 NOT NULL default 'wip',
-  `versiondisp` varchar(64) character set utf8 NOT NULL,
-  `URL` text character set utf8 NOT NULL,
-  `UpdateURL` text character set utf8,
-  `available` enum('no','yes') character set utf8 NOT NULL default 'no',
-  PRIMARY KEY  (`build`)
+  `build` int(11) NOT NULL DEFAULT '0',
+  `date` date NOT NULL DEFAULT '0000-00-00',
+  `type` enum('wip','stable') CHARACTER SET utf8 NOT NULL DEFAULT 'wip',
+  `versiondisp` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `URL` text CHARACTER SET utf8 NOT NULL,
+  `UpdateURL` text CHARACTER SET utf8,
+  `available` enum('no','yes') CHARACTER SET utf8 NOT NULL DEFAULT 'no',
+  PRIMARY KEY (`build`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,7 +82,9 @@ CREATE TABLE `dus_core_update` (
   `URL` text NOT NULL,
   `fileDL` text NOT NULL,
   `size` int(11) NOT NULL,
-  PRIMARY KEY  (`buildfrom`,`buildto`)
+  `realsize` int(11) NOT NULL,
+  `hash` varchar(40) NOT NULL,
+  PRIMARY KEY (`buildfrom`,`buildto`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -101,19 +95,21 @@ CREATE TABLE `dus_core_update` (
 
 CREATE TABLE `dus_driver` (
   `name` varchar(12) NOT NULL,
-  `version` int(11) NOT NULL default '0',
-  `versiontype` enum('stable','unstable') NOT NULL default 'stable',
+  `version` int(11) NOT NULL DEFAULT '0',
+  `versiontype` enum('stable','unstable') NOT NULL DEFAULT 'stable',
   `description` text NOT NULL,
-  `dudi` tinyint(4) NOT NULL default '0',
+  `dudi` tinyint(4) NOT NULL DEFAULT '0',
   `versiondisp` varchar(32) NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
-  `size` int(11) NOT NULL default '0',
+  `size` int(11) NOT NULL DEFAULT '0',
   `comment` text NOT NULL,
   `commentFR` text NOT NULL,
-  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`name`,`version`),
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `realsize` bigint(20) NOT NULL DEFAULT '0',
+  `hash` varchar(40) NOT NULL,
+  PRIMARY KEY (`name`,`version`),
   KEY `dudi` (`dudi`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -124,13 +120,15 @@ CREATE TABLE `dus_driver` (
 --
 
 CREATE TABLE `dus_duppi` (
-  `versionfrom` int(11) NOT NULL default '0',
+  `versionfrom` int(11) NOT NULL DEFAULT '0',
   `version` int(11) NOT NULL,
   `versiondisp` varchar(32) NOT NULL,
   `URL` text NOT NULL,
   `fileDL` text NOT NULL,
   `size` int(11) NOT NULL,
-  PRIMARY KEY  (`versionfrom`,`version`)
+  `realsize` int(11) NOT NULL,
+  `hash` varchar(40) NOT NULL,
+  PRIMARY KEY (`versionfrom`,`version`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -141,19 +139,19 @@ CREATE TABLE `dus_duppi` (
 
 CREATE TABLE `dus_hyperripper` (
   `name` varchar(12) NOT NULL,
-  `version` int(11) NOT NULL default '0',
-  `versiontype` enum('stable','unstable') NOT NULL default 'stable',
+  `version` int(11) NOT NULL DEFAULT '0',
+  `versiontype` enum('stable','unstable') NOT NULL DEFAULT 'stable',
   `description` text NOT NULL,
-  `duhi` tinyint(4) NOT NULL default '0',
+  `duhi` tinyint(4) NOT NULL DEFAULT '0',
   `versiondisp` varchar(32) NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
-  `size` int(11) NOT NULL default '0',
+  `size` int(11) NOT NULL DEFAULT '0',
   `comment` text NOT NULL,
   `commentFR` text NOT NULL,
-  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`name`,`version`),
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`name`,`version`),
   KEY `duhi` (`duhi`),
   KEY `versiontype` (`versiontype`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -166,13 +164,13 @@ CREATE TABLE `dus_hyperripper` (
 
 CREATE TABLE `dus_servers` (
   `serverID` tinyint(4) NOT NULL,
-  `serverPriority` int(11) NOT NULL default '0',
-  `serverCountry` varchar(2) character set ascii NOT NULL default 'fr',
+  `serverPriority` int(11) NOT NULL DEFAULT '0',
+  `serverCountry` varchar(2) CHARACTER SET ascii NOT NULL DEFAULT 'fr',
   `serverURL` text NOT NULL,
   `serverName` varchar(128) NOT NULL,
-  `serverEnabled` enum('true','false') character set ascii NOT NULL default 'true',
-  `serverUsePaths` enum('true','false') NOT NULL default 'true',
-  PRIMARY KEY  (`serverID`),
+  `serverEnabled` enum('true','false') CHARACTER SET ascii NOT NULL DEFAULT 'true',
+  `serverUsePaths` enum('true','false') NOT NULL DEFAULT 'true',
+  PRIMARY KEY (`serverID`),
   KEY `serverPriority` (`serverPriority`,`serverEnabled`),
   KEY `serverUsePaths` (`serverUsePaths`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -185,16 +183,18 @@ CREATE TABLE `dus_servers` (
 
 CREATE TABLE `dus_translation` (
   `name` varchar(32) NOT NULL,
-  `version` tinyint(4) NOT NULL default '0',
-  `release` tinyint(4) NOT NULL default '0',
+  `version` tinyint(4) NOT NULL DEFAULT '0',
+  `release` tinyint(4) NOT NULL DEFAULT '0',
   `description` varchar(128) NOT NULL,
   `author` text NOT NULL,
   `URL` text NOT NULL,
   `file` text NOT NULL,
   `fileDL` text NOT NULL,
-  `size` int(11) NOT NULL default '0',
-  `date` date NOT NULL default '0000-00-00',
-  PRIMARY KEY  (`name`,`version`,`release`)
+  `size` int(11) NOT NULL DEFAULT '0',
+  `date` date NOT NULL DEFAULT '0000-00-00',
+  `realsize` int(11) NOT NULL,
+  `hash` varchar(40) NOT NULL,
+  PRIMARY KEY (`name`,`version`,`release`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -206,10 +206,10 @@ CREATE TABLE `dus_translation` (
 CREATE TABLE `dus_versions` (
   `dupfrom` int(11) NOT NULL,
   `dupto` int(11) NOT NULL,
-  `duci` int(11) NOT NULL default '0',
-  `dudi` int(11) NOT NULL default '0',
-  `duhi` int(11) NOT NULL default '0',
-  `translation` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`dupfrom`,`dupto`),
+  `duci` int(11) NOT NULL DEFAULT '0',
+  `dudi` int(11) NOT NULL DEFAULT '0',
+  `duhi` int(11) NOT NULL DEFAULT '0',
+  `translation` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`dupfrom`,`dupto`),
   KEY `convert` (`duci`,`dudi`,`duhi`,`translation`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;

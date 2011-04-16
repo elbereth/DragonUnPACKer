f149867fc790da227cf016b9afed823477ec1eec
elbereth <elbereth@d7b88232-77aa-4384-a92c-d688c1a3d038>
2011-04-16 09:28:12 +0000

    Missing dependency fix...

---
+++ trunk/DragonUnPACKer/common/lib_temptools.pas
@@ -0,0 +1,51 @@
+unit lib_temptools;
+
+interface
+
+function getTemporaryDir(): string;
+function getTemporaryFilename(suffix: string = ''; prefix: string = 'dup5tmp'; separator: string = '-'): string;
+
+implementation
+
+uses Registry, Windows, SysUtils, lib_BinUtils;
+
+function getTemporaryDir(): string;
+var Reg: TRegistry;
+    TempDir: array[0..MAX_PATH] of Char;
+    testDir: string;
+begin
+
+  GetTempPath(MAX_PATH, @TempDir);
+  result := Strip0(TempDir);
+
+  Reg := TRegistry.Create;
+  Try
+    Reg.RootKey := HKEY_CURRENT_USER;
+    if Reg.OpenKey('\Software\Dragon Software\Dragon UnPACKer 5\StartUp',True) then
+    begin
+      if Reg.ValueExists('UseAltTempDir') and Reg.ReadBool('UseAltTempDir') and Reg.ValueExists('AltTempDir') then
+      begin
+        testDir := Reg.ReadString('AltTempDir');
+        if DirectoryExists(testDir) then
+          result := testDir;
+      end;
+      Reg.CloseKey;
+    end;
+  Finally
+    FreeAndNil(Reg);
+  end;
+
+end;
+
+function getTemporaryFilename(suffix: string = ''; prefix: string = 'dup5tmp'; separator: string = '-'): string;
+begin
+
+  randomize;
+  result := prefix+inttostr(GetTickCount)+separator+IntToStr(Random(99999999));
+  if (suffix <> '') then
+    result := result + separator + suffix;
+
+end;
+
+end.
+ 

32cfafebafd097b1d954ac963dd4fa45a0edd35a
Alexandre Devilliers <elbereth@dashninja.pl>
2014-04-21 20:41:40 +0000

    Initial version of Duppi v4.0.0: * Started from scratch with Lazarus 1.2 * This initial version goes until the connection to the DUS * Uses Indy10 (with a dirty hack for TLS SNI...) * Uses SSL to connect to the new update server at https://update.dragonunpacker.com/dus.php

---
+++ DragonUnPACKer/tools/duppi4/class_duppiinternet.pas
@@ -0,0 +1,22 @@
+unit class_DuppiInternet;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+
+TDuppiInternet = class(TObject)
+private
+  { private declarations }
+public
+  { public declarations }
+end;
+
+implementation
+
+end.
+
